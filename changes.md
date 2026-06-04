fac2e91246bf7405fc25c0e84493ee7274b87db5
Joe Marchionna <joemarchionna@gmail.com>
2026-06-03 23:18:17 -0400

    Added createMissingDir parameter to pidFile decorator v1.3.0

--- changes.md
+++ changes.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [Unreleased]
 none
 
+## [1.3.0] - 2026 06 03
+### Added
+- createMissingDir parameter to pidFile decorator
+
 ## [1.2.0] - 2026 05 07
 ### Added
 - methodParameter decorators
--- kisspy/decorators/pidFile.py
+++ kisspy/decorators/pidFile.py
@@ -1,14 +1,19 @@
 from kisspy.exceptions import PidFilePresentException
+from kisspy.xtdPy.paths.directories import createDir
 from functools import wraps
 import os
 
 PID_FN = "wkdir/appPID.txt"
 
 
-def pidFile(filename: str = PID_FN):
+def pidFile(filename: str = PID_FN, createMissingDir: bool = True):
     """
-    will allow method decorated to run only if the pid file specified is not present\n
+    will allow method decorated to run only if the pid file specified is not present
     if filename is set to None this will have no effect (will allow multiple instances)
+
+    Args:
+        filename (str, optional): PID filename. Defaults to 'wkdir/appPID.txt'. If set to None, disables this check
+        createMissingDir (bool, optional): If True, creates the filename directory if not exists. Defaults to True
     """
 
     def pidFileDecorator(func):
@@ -18,6 +23,8 @@ def pidFile(filename: str = PID_FN):
                 if filename:
                     if os.path.exists(filename):
                         raise PidFilePresentException("Method Already Running!")
+                    if createMissingDir:
+                        createDir(fqFilename=filename)
                     with open(filename, "w") as wtr:
                         wtr.write("{}\n".format(os.getpid()))
                 results = func(*args, **kwargs)
